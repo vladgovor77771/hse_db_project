b4843a0e6084bde468408c02220f33f0391e7712
vladgovor77771 <qwertystepp@yandex.ru>
2022-12-17 21:17:22 +0300

    fix

--- migration.sql
+++ migration.sql
@@ -24,96 +24,96 @@ CREATE TYPE "message_status" AS ENUM (
 );
 
 CREATE TABLE "personal_datas" (
-  "id" serial PRIMARY KEY,
-  "first_name" varchar,
-  "last_name" varchar,
+  "id" serial PRIMARY KEY NOT NULL,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
   "birthday" date,
-  "phone_number" varchar
+  "phone_number" varchar NOT NULL
 );
 
 CREATE TABLE "cars" (
-  "id" serial PRIMARY KEY,
-  "number" varchar,
-  "brand" varchar,
-  "model" varchar,
-  "manufacture_year" integer
+  "id" serial PRIMARY KEY NOT NULL,
+  "number" varchar NOT NULL,
+  "brand" varchar NOT NULL,
+  "model" varchar NOT NULL,
+  "manufacture_year" integer NOT NULL
 );
 
 CREATE TABLE "drivers" (
-  "id" serial PRIMARY KEY,
-  "personal_data_id" integer,
-  "car_id" integer,
-  "driver_license_number" varchar
+  "id" serial PRIMARY KEY NOT NULL,
+  "personal_data_id" integer NOT NULL,
+  "car_id" integer NOT NULL,
+  "driver_license_number" varchar NOT NULL
 );
 
 CREATE TABLE "customers" (
-  "id" serial PRIMARY KEY,
-  "personal_data_id" integer
+  "id" serial PRIMARY KEY NOT NULL,
+  "personal_data_id" integer NOT NULL
 );
 
 CREATE TABLE "items" (
-  "id" serial PRIMARY KEY,
-  "name" varchar,
-  "description" varchar,
-  "length" double precision,
-  "width" double precision,
-  "height" double precision,
-  "weight" double precision,
+  "id" serial PRIMARY KEY NOT NULL,
+  "name" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "length" double precision NOT NULL,
+  "width" double precision NOT NULL,
+  "height" double precision NOT NULL,
+  "weight" double precision NOT NULL,
   "min_age" integer
 );
 
 CREATE TABLE "points" (
-  "id" serial PRIMARY KEY,
-  "longitude" double precision,
-  "latitude" double precision,
-  "address" varchar
+  "id" serial PRIMARY KEY NOT NULL,
+  "longitude" double precision NOT NULL,
+  "latitude" double precision NOT NULL,
+  "address" varchar NOT NULL
 );
 
 CREATE TABLE "waybills" (
-  "id" serial PRIMARY KEY,
-  "driver_id" integer
+  "id" serial PRIMARY KEY NOT NULL,
+  "driver_id" integer NOT NULL
 );
 
 CREATE TABLE "waybill_points" (
-  "point_id" integer,
-  "order_id" integer,
-  "waybill_id" integer,
-  "type" point_type,
-  "visit_order" integer,
-  "visited" boolean,
+  "point_id" integer NOT NULL,
+  "order_id" integer NOT NULL,
+  "waybill_id" integer NOT NULL,
+  "type" point_type NOT NULL,
+  "visit_order" integer NOT NULL,
+  "visited" boolean NOT NULL DEFAULT FALSE,
   PRIMARY KEY ("order_id", "waybill_id", "type")
 );
 
 CREATE TABLE "orders" (
-  "id" serial PRIMARY KEY,
-  "customer_id" integer,
-  "source_point_id" integer,
-  "delivery_point_id" integer,
-  "return_point_id" integer,
+  "id" serial PRIMARY KEY NOT NULL,
+  "customer_id" integer NOT NULL,
+  "source_point_id" integer NOT NULL,
+  "delivery_point_id" integer NOT NULL,
+  "return_point_id" integer NOT NULL,
   "waybill_id" integer,
-  "status" order_status
+  "status" order_status NOT NULL
 );
 
 CREATE TABLE "order_items" (
-  "item_id" integer,
-  "order_id" integer,
-  "number" integer,
+  "item_id" integer NOT NULL,
+  "order_id" integer NOT NULL,
+  "number" integer NOT NULL,
   PRIMARY KEY ("item_id", "order_id")
 );
 
 CREATE TABLE "chats" (
-  "id" serial PRIMARY KEY,
-  "order_id" integer,
-  "updated_at" timestamptz
+  "id" serial PRIMARY KEY NOT NULL,
+  "order_id" integer NOT NULL,
+  "updated_at" timestamptz NOT NULL
 );
 
 CREATE TABLE "chat_messages" (
-  "id" serial PRIMARY KEY,
-  "chat_id" integer,
-  "person_id" integer,
-  "message" varchar,
-  "created_at" timestamptz,
-  "status" message_status
+  "id" serial PRIMARY KEY NOT NULL,
+  "chat_id" integer NOT NULL,
+  "person_id" integer NOT NULL,
+  "message" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL,
+  "status" message_status NOT NULL
 );
 
 ALTER TABLE "waybill_points" 
@@ -166,5 +166,5 @@ CREATE TYPE waybill_point AS (
   "order_id" integer,
   "waybill_id" integer,
   "type" point_type,
-  "visit_order" integer,
+  "visit_order" integer
 );
